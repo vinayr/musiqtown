997ccbfc1ef4177914c5e090563a618b90a15463
Vinay <vinay.rv@gmail.com>
2013-12-20 19:48:43 +0900

    Update popup.js

--- javascript/popup.js
+++ javascript/popup.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
 	
-	UI.stylishForms();
-	
 	Events.enableFocusCall();
 	Events.submitFormOnEnterAndAutoSearchArrows();
 	
 	$('#searchField').focus();
 	$('#searchField').keyup(Events.typingInSearchField);
 	$('#searchButton').click(Events.submitForm);
-
+	
 });
